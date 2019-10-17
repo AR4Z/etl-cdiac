86b3113595913b5002ada9bdc4aacc8e9568fc60
Orlando Hernandez <oaraz02@gmail.com>
2019-10-17 08:27:19 -0500

    add user to usersSeeds

--- database/seeds/user/UserSeeder.php
+++ database/seeds/user/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
             [
 
                 [
-                    # id => 1
+                    'id'                        => 1,
                     'name'                      => 'pruebas',
                     'lastname'                  => 'pruebas',
                     'email'                     => 'lflondonor@gmail.com',
@@ -26,6 +26,18 @@ class UserSeeder extends Seeder
                     'active'                    => TRUE,
                     'confirm'                   => TRUE,
                     'confirmed_code'            => '912044',
+                    'email_verified_at'         => '16/10/2019'
+                ]
+            ]
+        );
+        DB::connection('public')->table('role_application')->insert(
+            [
+
+                [
+                    'id'                        => 1,
+                    'role_id'                   => 1,
+                    'application_id'            => 1,
+                    'user_id'                   => 1
                 ]
             ]
         );
